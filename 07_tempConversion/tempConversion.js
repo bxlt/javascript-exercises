6b9135aa39cb98296836a133e1279896387ffbe1
bxlt <xuelitan123@gmail.com>
2024-04-10 22:49:33 -0400

    completed js exe 1-7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+function round(num){
+  return Math.round(num*10)/10
+}
+
+const convertToCelsius = function(tempK) {
+  return round((tempK-32)/1.8)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+
+  return round(tempC*1.8+32)
 };
 
 // Do not edit below this line
